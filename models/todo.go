42ec5a0d739b6c67c85cf43fbb9dfb706f4bdf37
mardoqueu.telvina <mardoqueu.telvina@cibernix.com>
2024-10-16 09:31:31 -0300

    feat: Add immediate response for newly created todo, enhance todo handling in API

--- models/todo.go
+++ models/todo.go
@@ -6,13 +6,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// struct
 type Todo struct {
-	ID        uint   `gorm:"primaryKey" json:"id" `
+	ID        uint   `gorm:"primaryKey" json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
 
+// Handler to get all todos
 func GetTodos(c *fiber.Ctx) error {
 	db := database.DBConn
 	var todos []Todo
@@ -20,68 +20,76 @@ func GetTodos(c *fiber.Ctx) error {
 	return c.JSON(&todos)
 }
 
+// Handler to get a todo by ID
 func GetTodoById(c *fiber.Ctx) error {
 	id := c.Params("id")
 	db := database.DBConn
 	var todo Todo
+	// Attempt to find the todo by ID
 	err := db.Find(&todo, id).Error
 	if err != nil {
-		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Could not find todo", "data": err})
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Todo not found", "data": err})
 	}
 	return c.JSON(&todo)
 }
 
+// Handler to create a new todo
 func CreateTodo(c *fiber.Ctx) error {
 	db := database.DBConn
 	todo := new(Todo)
+
+	// Try to parse the request body into the Todo struct
 	err := c.BodyParser(todo)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Check your input", "data": err})
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Cannot parse JSON", "data": err})
 	}
+
+	// Save the todo to the database
 	err = db.Create(&todo).Error
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Check your inp", "data": err})
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Could not create Todo", "data": err})
 	}
-	return c.JSON(&todo)
 
+	// Return the new todo with the generated ID
+	return c.JSON(todo)
 }
 
+// Handler to update an existing todo
 func UpdateTodo(c *fiber.Ctx) error {
-	type UpdatedTodo struct {
-		Title     string `json:"title"`
-		Completed bool   `json:"completed"`
-	}
-	id := c.Params("id")
+	id := c.Params("id") // Get the todo ID from the URL
 	db := database.DBConn
 	var todo Todo
+
+	// Try to find the todo with the given ID
 	err := db.Find(&todo, id).Error
 	if err != nil {
-		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Could not find todo", "data": err})
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Todo not found", "data": err})
 	}
 
-	var updatedTodo UpdatedTodo
-	err = c.BodyParser(&updatedTodo)
-
+	// Parse the request body to update the todo
+	err = c.BodyParser(&todo)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Review your input", "data": err})
+		return c.Status(500).JSON(fiber.Map{"status": "error", "message": "Cannot parse JSON", "data": err})
 	}
 
-	todo.Title = updatedTodo.Title
-	todo.Completed = updatedTodo.Completed
+	// Save the changes to the database
 	db.Save(&todo)
 	return c.JSON(&todo)
-
 }
 
+// Handler to delete a todo
 func DeleteTodo(c *fiber.Ctx) error {
 	id := c.Params("id")
 	db := database.DBConn
 	var todo Todo
+
+	// Try to find the todo with the given ID
 	err := db.Find(&todo, id).Error
 	if err != nil {
-		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Could not find todo", "data": err})
+		return c.Status(404).JSON(fiber.Map{"status": "error", "message": "Todo not found", "data": err})
 	}
 
+	// Delete the found todo
 	db.Delete(&todo)
-	return c.Status(200).JSON(fiber.Map{"status": "success", "message": "Todo successfully deleted"})
+	return c.Status(200).JSON(fiber.Map{"status": "success", "message": "Todo deleted"})
 }
